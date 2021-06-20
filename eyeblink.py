518c3a5335aebdaa2f73804f6e076c8b4331effd
madhur247 <64833453+madhur247@users.noreply.github.com>
2021-06-20 23:37:15 +0530

    Update eyeblink.py

--- eyeblink.py
+++ eyeblink.py
@@ -2,43 +2,43 @@
 """
 Created on Sat Jun  5 18:22:22 2021
 
-@author: madhu
+@author: madhur
 """
-import time
+import time            
 from scipy.spatial import distance as dist
 import dlib
 import cv2
 def eye_aspect_ratio(eye):
-    A = dist.euclidean(eye[1],eye[5])
-    B = dist.euclidean(eye[2],eye[4])
-    C = dist.euclidean(eye[0],eye[3])
-    asratio = (A+B)/(2*C)
+    A = dist.euclidean(eye[1],eye[5])                       #distance between top and bottom points
+    B = dist.euclidean(eye[2],eye[4])                       #distance between top and bottom points
+    C = dist.euclidean(eye[0],eye[3])                       #distance between left and right points
+    asratio = (A+B)/(2*C)                                   #calculating the aspect ratio
     return asratio
         
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontcolor = (0,255,0)
 fontscale = 1
 face_arr = []
-cap = cv2.VideoCapture(0)
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
-end = time.time() + 15
-while time.time() < end:
-    res, img = cap.read()
-    img = cv2.flip(img,1)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = detector(gray,1)
-    if len(faces)>len(face_arr):
+cap = cv2.VideoCapture(0)        #turn on camera (since 0 means in-built camera of your laptop)
+detector = dlib.get_frontal_face_detector()      #detect faces
+predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')     #detect face landmarks
+end = time.time() + 15           #it will run for 15 secs
+while time.time() < end:           #while loop for 15 secs
+    res, img = cap.read()        #read camera input
+    img = cv2.flip(img,1)        #flipping of the input image/frame (so that it doesn't appear like a mirror)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)           #converting the input to grayscale(required for detecting faces)
+    faces = detector(gray,1)                               # getting an array of face bounding box co-ordinates
+    if len(faces)>len(face_arr):                           #this if block ensures for each frame the same faces aren't appended again and again for each frame
         for i in range(len(faces)-len(face_arr)):
             face_arr.append([0,0,"Fake"])
-    for (i,face) in enumerate(faces):
+    for (i,face) in enumerate(faces):                      #this takes care of all the faces detected
         x = face.left()
         y = face.top()
         w = face.right()
         h = face.bottom()
-        landm = predictor(gray,face).parts()
-        '''i = 36
-        while i<41:
+        landm = predictor(gray,face).parts()               #array of face landmarks
+        '''i = 36                                          # documented this part because I got a shorter version of the same code below
+        while i<41:                                        #You will understand this piece of code after seeing the face landmarks image 
             pt = landm[i]
             pt2 = landm[i+1]
             cv2.line(img,(pt.x,pt.y),(pt2.x,pt2.y),(0,255,0),1)
@@ -51,21 +51,21 @@ while time.time() < end:
             cv2.line(img,(pt.x,pt.y),(pt2.x,pt2.y),(0,255,0),1)
             i+=1
         cv2.line(img,(landm[42].x,landm[42].y),(landm[47].x,landm[47].y),(0,255,0),1)'''
-        cv2.rectangle(img,(x,y),(w,h),(0,255,0),2)
-        left = [[p.x,p.y] for p in landm[36:42]]
-        right = [[p.x,p.y] for p in landm[42:48]]
-        leftear = eye_aspect_ratio(left)
-        rightear = eye_aspect_ratio(right)
-        ear = (leftear+rightear)/2
-        print(ear)
-        if ear < 0.265:
+        cv2.rectangle(img,(x,y),(w,h),(0,255,0),2)                       #creates a rectangle according to face co-ordinates
+        left = [[p.x,p.y] for p in landm[36:42]]                         #extracts left eye
+        right = [[p.x,p.y] for p in landm[42:48]]                        #extracts right eye
+        leftear = eye_aspect_ratio(left)                                 #calculates left eye aspect ratio
+        rightear = eye_aspect_ratio(right)                               #calculates right eye aspect ratio
+        ear = (leftear+rightear)/2                                       # takes an average of the eye ratios 
+        #print(ear)                                                      #this is optional (only for getting the right eye aspect ratio threshold)
+        if ear < 0.265:                                                  #this threshold could be different for different people
             face_arr[i][0]+=1
         else:
-            if face_arr[i][0]>=2:
+            if face_arr[i][0]>=2:                                        #if eye ratio is less than threshold for 2 frames the it's a blink
                 face_arr[i][1]+=1
                 face_arr[i][0]=0
         if face_arr[i][1]>=2:
-            face_arr[i][2]="Real"
+            face_arr[i][2]="Real"                                        #if the face blinks more than twice it's a real face
         cv2.putText(img,face_arr[i][2],(x,h),font,fontscale,fontcolor,3)
         cv2.putText(img,"Blinks="+str(face_arr[i][1]),(x,y),font,fontscale,fontcolor,3)
     print(face_arr)
